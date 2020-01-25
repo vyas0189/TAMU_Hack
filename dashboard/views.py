38dc02a982a54beae93dffde584a6898fc3e1712
Chantha Mak <chantha.mak17@gmail.com>
2020-01-25 16:27:09 -0600

    added html file

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'dashboards/index.html')
 
 # testing /////////////////////////////////////////////////////////////
 

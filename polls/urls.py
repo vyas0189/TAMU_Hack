057af812deaa3550dfe5cb282a99de868ad44dac
jesus <36487891+DelRiego@users.noreply.github.com>
2020-01-25 14:57:36 -0600

    Fixed polls

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]

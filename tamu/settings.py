687abb4b7bf3536873b8ae5649412bc7c9580d75
Chantha Mak <chantha.mak17@gmail.com>
2020-01-25 19:53:07 -0600

    added bootstrap

--- tamu/settings.py
+++ tamu/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'flightInfoPage',
     'dashboard',
     'django.contrib.admin',
     'django.contrib.auth',
--- tamu/urls.py
+++ tamu/urls.py
@@ -20,6 +20,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name="login/index.html")),
     path('dashboard/', include('dashboard.urls')),
+    path('flightInfoPage/', include('flightInfoPage.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls'))
 ]

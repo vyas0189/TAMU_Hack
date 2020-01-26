505639d49760dbb425c0ac76d2c9550d95b141b0
mmabbas2 <muhammad98abbas@gmail.com>
2020-01-25 20:52:21 -0600

    lolz

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,17 +6,3 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'dashboards/layout.html')
 
-# testing /////////////////////////////////////////////////////////////
-
-
-def detail(request, question_id):
-    return HttpResponse("Chantha likes that %s meat." % question_id)
-
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)

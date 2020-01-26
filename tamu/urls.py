5caf88112537a2475b0fb85cf859dcef38d6f142
vyas0189 <vyas01890@outlook.com>
2020-01-25 19:23:13 -0600

    added logout

--- tamu/urls.py
+++ tamu/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
+from login import views as login_view
+
 urlpatterns = [
     path('', TemplateView.as_view(template_name="login/index.html")),
-    path('dashboard/', include('dashboard.urls')),
+    path('dashboard/', include('login.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls'))
 ]
